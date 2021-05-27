d966f07da36da10f5a85f979b3f5f7845174d6cb
ZhouZhaoJian <bberzhou@gmail.com>
2021-05-27 17:24:23 +0800

    function changes

--- Functions/FirstFun.py
+++ Functions/FirstFun.py
@@ -1,11 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import math
+# from XXXA import XXXB ，就是从XXXA引入XXXB函数
 
+# 一、调用函数
 #  python 库自带的函数，可以直接调用，参考文档 https://docs.python.org/3/library/functions.html
 print(max(1, 3))
-
+# 调用函数的时候，如果传入的参数数量不对，会报TypeError的错误，并且Python会明确地告诉你：abs()有且仅有1个参数，但给出了两个
 
 # 在Python中，定义一个函数要使用def语句，依次写出函数名、括号、括号中的参数和冒号:，
 # 然后，在缩进块中编写函数体，函数的返回值用return语句返回
+
 
 def my_abs(x):
     if x >= 0:
@@ -51,4 +56,73 @@ print(r)
 
 
 # 其实返回的是一个(151.96152422706632, 70.0)  tuple
+# 原来返回值是一个tuple！但是，在语法上，返回一个tuple可以省略括号，
+# 而多个变量可以同时接收一个tuple，按位置赋给对应的值，
+# 所以，Python的函数返回多值其实就是返回一个tuple，但写起来更方便
+# 二、数据类型转换
+# Python内置的常用函数还包括数据类型转换函数，比如int()函数可以把其他数据类型转换为整数
+print(int('123'))
+print(float('12.34'))
+print(str(1.23))    # 这里输出是字符串：1.23
+
+# 利用Python内置的hex()函数把一个整数转换成十六进制表示的字符串
+n1 = 255
+print(hex(n1))  # 0xff
+
+
+# 三、空函数
+# 如果想定义一个什么事也不做的空函数，可以用pass语句
+def nops():
+    pass
+
+# 四、参数检查
+# 调用函数时，如果参数个数不对，Python解释器会自动检查出来，并抛出TypeError
+
+# my_abs1(1, 2)，传入两个参数就会报错
+# 数据类型检查可以用内置函数isinstance()实现
+
+
+def my_abs2(x):
+    # 先判断传入的类型
+    if not isinstance(x, (int, float)):
+        # 如果不是int或者float类型，就会报错
+        raise TypeError('bad operand type')
+    if x >= 0:
+        return x
+    else:
+        return -x
+
+
+'''
+    小结：
+    定义函数时，需要确定函数名和参数个数；
+
+    如果有必要，可以先对参数的数据类型做检查；
+
+    函数体内部可以用return随时返回函数结果；
+
+    函数执行完毕也没有return语句时，自动return None。
+
+    函数可以同时返回多个值，但其实就是一个tuple。    
+'''
+
+'''
+  请定义一个函数quadratic(a, b, c)，接收3个参数，返回一元二次方程 ax^2+bx+c=0的两个解。
+  利用求根公式
+    
+'''
+
+
+def quadratic(a, b, c):
+    delt = b**2 - 4*a*c
+    if delt < 0:
+        print('equation is no answer!%d'%delt)
+        return ('测试失败，该方程无解')
+    else:
+        x1 = (-b + math.sqrt(delt))/(2*a)
+        x2 = (-b - math.sqrt(delt))/(2*a)
+    return x1, x2
+
+
+print(quadratic(1, 3, -4))
 
--- PythonFundamentals/dictAndSet.py
+++ PythonFundamentals/dictAndSet.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Python内置了字典：dict的支持，dict全称dictionary，在其他语言中也称为map，使用键-值（key-value）存储，具有极快的查找速度
 
@@ -7,15 +8,21 @@ print(d['michael'])  # 97
 # 把数据放入dict的方法，除了初始化时指定外，还可以通过key放入
 
 d['Adam'] = 78
-print(d['Adam'])
-
+print(d['Adam'])    # 78
+# 一个key只能对应一个value，所以，多次对一个key放入value，后面的值会把前面的值冲掉
+d['Adam'] = 80
+print(d['Adam'])    # 80
 # 由于一个key只能对应一个value，所以，多次对一个key放入value，后面的值会把前面的值覆盖掉
+
 # 如果key不存在，dict就会报错
-# 要避免key不存在的错误，有两种办法，一是通过in判断key是否存在,会返回一个布尔值，
+# 要避免key不存在的错误，有两种办法:
+# 一是通过in判断key是否存在,会返回一个布尔值,
+flag = 'Adam' in d
+print(flag)     # True
 # 二是通过dict提供的get()方法，如果key不存在，可以返回None，或者自己指定的value
 print(d.get('Th', ))  # None
 
-print(d.get('Th', -1))  # -1,返回的指定值
+print(d.get('Th', -1))  # -1,返回指定的值
 
 # 要删除一个key，用pop(key)方法，对应的value也会从dict中删除掉
 d.pop('Bob')
@@ -26,22 +33,24 @@ print(d)  # 删除Bob,后{'michael': 97, 'Tracy': 86, 'Adam': 78}
 # list和dict的比较和list比较，
 # dict有以下几个特点：
 #
-# 1、查找和插入的速度极快，不会随着key的增加而变慢；
-# 2、需要占用大量的内存，内存浪费多。
+#   1、查找和插入的速度极快，不会随着key的增加而变慢；
+#   2、需要占用大量的内存，内存浪费多。
 
 # 而list相反：
 #
 # 1、查找和插入的时间随着元素的增加而增加；
 # 2、占用空间小，浪费内存很少。
-# 所以，dict是用空间来换取时间的一种方法。
+# 所以，dict是用空间来换取时间的一种方法。要注意：dict里面的key必须是不可变的对象
+# 在Python中，字符串、整数等都是不可变的
 # 这是因为dict根据key来计算value的存储位置，
 # 如果每次计算相同的key得出的结果不同，那dict内部就完全混乱了。
 # 这个通过key计算位置的算法称为哈希算法（Hash）
 # 在Python中，字符串、整数等都是不可变的，因此，可以放心地作为key。
-# 而list是可变的，就不能作为key，例如
+# 注意：list是可变的，就不能作为key，例如
 key = [1, 2, 3]
 # d[key] = 'a list ' TypeError: unhashable type: 'list'
 
+# 二、set
 # set和dict类似，也是一组key的集合，但不存储value。
 # 由于key不能重复，所以，在set中，没有重复的key。要创建一个set，需要提供一个list作为输入集合
 
@@ -51,10 +60,11 @@ print(s)  # {1, 2, 3}
 s1 = {'jjhh', 'gg', 'g'}
 print(s1)  # {'jjhh', 'gg', 'g'}
 
-# 传入的参数[1, 2, 3]是一个list，而显示的{1, 2, 3}只是告诉你这个set内部有1，2，3这3个元素，显示的顺序也不表示set是有序的。。
-# 并且重复元素在set中自动被过滤
+# 传入的参数[1, 2, 3]是一个list，而显示的{1, 2, 3}只是告诉你这个set内部有1，2，3这3个元素，
+# 显示的顺序也不表示set是有序的。并且重复元素在set中自动被过滤
+
 s3 = {1, 2, 3, 4, 5, 1, 3, 4}
-print(s3)  # {1, 2, 3, 4, 5}相同的元素自动过滤掉
+print(s3)  # 输出set，{1, 2, 3, 4, 5}相同的元素自动过滤掉
 
 # 通过add(key)方法可以添加元素到set中，可以重复添加，但不会有效果
 s3.add(2)
@@ -66,9 +76,24 @@ print(s3)  # 输出结果  {1, 2, 3, 5}
 
 s4 = {1, 2, 3}
 s5 = {2, 3, 4}
-print(s4 & s5)  # {2, 3}
+print(s4 & s5)  # {2, 3} 求交集
+
+s6 = {1, 2, 4}
+s7 = {1, 4, 0}
+print(s6 | s7)  # {0, 1, 2, 4}，求并集
 
 # set和dict的唯一区别仅在于没有存储对应的value，
 # 但是，set的原理和dict一样，所以，同样不可以放入可变对象，
 # 因为无法判断两个可变对象是否相等，也就无法保证set内部“不会有重复元素”。
 # 试试把list放入set，看看是否会报错
+# s8 = {[1, 2, 4]}, 将一个List放入set就报错
+# print(s8)
+# s8 = {[1, 2, 4]}
+# TypeError: unhashable type: 'list'
+
+# 对于不变对象来说，调用对象自身的任意方法，也不会改变该对象自身的内容。
+# tuple元组虽然是不可变对象，但是放入的元素可以是List，所以还是可以变
+s9 = {(1, 2, [2, 3])}
+print(s9)
+#     s9 = {(1, 2, [2, 3])}
+# TypeError: unhashable type: 'list'
